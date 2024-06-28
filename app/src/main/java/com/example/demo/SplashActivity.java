52597f126556b0110a42d227ac32049a1c319243
vishalbhut <bhutvishal696@gmail.com>
2024-06-28 14:54:19 +0530

    a

---
+++ app/src/main/java/com/example/demo/SplashActivity.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+    }
+}
